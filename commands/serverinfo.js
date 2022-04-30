f610e4c3e82dc25bf54cb3042ae2a4d427d59b93
samast2010 <66264481+samast2010@users.noreply.github.com>
2022-04-30 14:45:16 +0100

    Server info command

---
+++ commands/serverinfo.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed, CommandInteraction } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('serverinfo')
+		.setDescription('Tells you the info of the server you\'re in!'),
+
+	/**
+	 * @param {CommandInteraction} interaction 
+	 */
+	async execute(interaction) {
+		await interaction.deferReply();
+		const { guild } = interaction;
+		const embed3 = new MessageEmbed()
+			.setAuthor({ name: guild.name, iconURL: guild.iconURL({ dynamic: true }) })
+			.setThumbnail(guild.iconURL({ dynamic: true }))
+			.setColor('BLURPLE')
+			.addFields(
+				{
+					name: 'General',
+					value: [
+						`Created: <t:${parseInt(guild.createdTimestamp / 1000)}:R>`,
+						`Owner: <@${guild.ownerId}>`,
+						`\nDescription: **${guild.description || 'None'}**`
+					].join('\n')
+				},
+				{
+					name: 'Users',
+					value: [
+						`- Members: ${guild.memberCount}`
+					].join('\n')
+				},
+				{
+					name: 'Channels',
+					value: [
+						`- Text: ${guild.channels.cache.filter((c) => c.type === 'GUILD_TEXT').size}`,
+						`- Voice: ${guild.channels.cache.filter((c) => c.type === 'GUILD_VOICE').size}`,
+						`- Threads: ${guild.channels.cache.filter((c) => c.type === 'GUILD_PUBLIC_THREAD' && 'GUILD_PRIVATE_THREAD' && 'GUILD_NEWS_THREAD').size}`,
+						`- Categories: ${guild.channels.cache.filter((c) => c.type === 'GUILD_CATEGORY').size}`,
+						`- Stages: ${guild.channels.cache.filter((c) => c.type === 'GUILD_STAGE_VOICE').size}`,
+						`- News: ${guild.channels.cache.filter((c) => c.type === 'GUILD_NEWS').size}`,
+						`Total: ${guild.channels.cache.size}`
+					].join('\n')
+				},
+				{
+					name: 'Emojis and Stickers',
+					value: [
+						`Animated: ${guild.emojis.cache.filter((e) => e.animated).size}`,
+						`Static: ${guild.emojis.cache.filter((e) => !e.animated).size}`
+					].join('\n')
+				}
+			)
+
+		await interaction.editReply({ content: 'Here you go!', embeds: [embed3] });
+	},
+};
